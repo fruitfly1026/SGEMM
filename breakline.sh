9f2fd697738f6d3181d8bfc3082cd27ecb216b74
Xiuxia Zhang <zhangxiuxia1@gmail.com>
2016-07-29 12:20:05 +0800

    add line break -s option to breakline.sh

--- breakline.sh
+++ breakline.sh
@@ -1,6 +1,6 @@
 for f in benchmark.tex  conclusion.tex  evaluation.tex Introduction.tex methodology.tex optimization.tex related.tex solver.tex
 do
     echo $f
-    cat $f |fold -w 120 > tmp.tex
+    cat $f |fold -s -w 120 > tmp.tex
     mv tmp.tex $f
 done
