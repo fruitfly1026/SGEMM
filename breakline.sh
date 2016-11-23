6516288f3a5ff521a3e316e6e470e77b218735a3
Xiuxia Zhang <zhangxiuxia1@gmail.com>
2016-11-23 17:04:31 +0800

    grammar error fixed by Fanjiang Zeng

--- breakline.sh
+++ breakline.sh
@@ -1,4 +1,5 @@
-for f in benchmark.tex  conclusion.tex  evaluation.tex Introduction.tex methodology.tex optimization.tex related.tex solver.tex
+for f in text/discuss.tex
+#benchmark.tex  conclusion.tex  evaluation.tex Introduction.tex methodology.tex optimization.tex related.tex solver.tex
 do
     echo $f
     cat $f |fold -s -w 120 > tmp.tex
